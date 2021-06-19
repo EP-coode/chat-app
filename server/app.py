cf5d5a2dfbc0c2b5324d4fd0a2cc0cec1f4afe0d
Ernest Przybyl <ernestprzybyl@gmail.com>
2021-06-19 12:50:09 +0200

    merge db_models with database, prepare login logic

--- server/app.py
+++ server/app.py
@@ -1,15 +1,56 @@
+import datetime
+import hashlib
 import os
 
-from flask import Flask
+from flask import Flask, request
+from sqlalchemy.orm.session import Session
+import jwt
 
-import database
+from database import User, DB_Session
+from db_operations import isLoginAvalible, loginUser, registerUser
 
 app = Flask(__name__)
+token_secret = os.getenv("TOKEN_SECRET", "yoursuperstrongpassword123")
 
 
 @app.route('/ping', methods=['GET'])
 def pong():
-    return 'pong'
+    return 'pong', 200
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    if not 'login' in data or not 'password' in data:
+        return 'Bad request', 400
+
+    if not isLoginAvalible(data['login']):
+        return 'User with this login already exist', 409
+
+    registerUser(data['login'], data['password'])
+
+    return 'Account created', 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not 'login' in data or not 'password' in data:
+        return 'Bad request', 400
+
+    user = loginUser(data['login'], data['password'])
+    if not user:
+        return 'Password or login is worong', 403
+
+    token = jwt.encode({
+        'id': user.id,
+        'user': user.login,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    }, token_secret)
+
+    return {"token": token}, 200
 
 
 if __name__ == "__main__":
--- server/database.py
+++ server/database.py
@@ -1,25 +1,85 @@
+import datetime
+import hashlib
 import os
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, create_engine
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
+
+# ========= database connection =========
 db_user = os.getenv("db_user", "root")
-db_password = os.getenv("db_password", "example_password")
-db_host = os.getenv("db_host", "0.0.0.0")
+db_password = os.getenv("db_password", "example")
+db_host = os.getenv("db_host", "127.0.0.1")
 db_port = os.getenv("db_port", "3306")
 
 engine = create_engine("mysql+pymysql://{}:{}@{}:{}".format(
     db_user, db_password, db_host, db_port
 ))
 
-# engine = create_engine(f'sqlite:///messenger.db')
-
-# try to get connection to database
-# if cannot program will die here
 engine.execute(r"CREATE DATABASE IF NOT EXISTS messenger")
 engine.execute(r"USE messenger")
 
-Base = declarative_base()
+DB_Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# ========= database models ============
+Base = declarative_base()
+Base.query = scoped_session(DB_Session).query_property()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String(32), unique=True, index=True)
+    hashed_password = Column(String(32))
+    last_activity = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, login: str, password: str) -> None:
+        self.login = login
+        self.hashed_password = hashlib.md5(password.encode()).hexdigest()
+
+
+class Chat(Base):
+    __tablename__ = "chats"
+
+    id = Column(Integer, primary_key=True, index=True)
+    creator = Column(Integer, ForeignKey(User.id))
+    messages = relationship("Message")
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String(256))
+    send_time = Column(DateTime, default=datetime.datetime.utcnow)
+    sender_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    target_chat_id = Column(Integer, ForeignKey(Chat.id), nullable=False)
+
+
+class MessageReadedBy(Base):
+    __tablename__ = "messages_readed_by"
+
+    message_id = Column(Integer, ForeignKey(Message.id),
+                        nullable=False, primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.id),
+                     nullable=False, primary_key=True)
+
+
+class ChatMembership(Base):
+    __tablename__ = "chat_memeberships"
+
+    chat_id = Column(Integer, ForeignKey(Chat.id),
+                     nullable=False, primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.id),
+                     nullable=False, primary_key=True)
+
+
+# temporary sollution for problem with not adding "IF NOT EXISTS" to sql executed by DB
+try:
+    Base.metadata.create_all(bind=engine)
+except:
+    pass
